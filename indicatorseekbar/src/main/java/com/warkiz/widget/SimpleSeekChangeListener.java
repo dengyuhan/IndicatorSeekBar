099fb4157440efcfe5ed630b0467929724d5eb95
dengyuhan <464958567@qq.com>
2018-06-19 14:51:04 +0800

    增加SimpleSeekChangeListener

---
+++ indicatorseekbar/src/main/java/com/warkiz/widget/SimpleSeekChangeListener.java
@@ -0,0 +1,18 @@
+package com.warkiz.widget;
+
+public class SimpleSeekChangeListener implements OnSeekChangeListener{
+    @Override
+    public void onSeeking(SeekParams seekParams) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(IndicatorSeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(IndicatorSeekBar seekBar) {
+
+    }
+}
